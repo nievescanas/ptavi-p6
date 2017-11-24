393223e55db4cfea7daece6734aa39bb50f0bd43
Nieves Canas Martin <nievescm@alpha29.aulas.gsyc.urjc.es>
2017-11-24 10:41:01 +0100

    Añadido mensajes rtp y pasado pep8

--- client.py
+++ client.py
@@ -3,37 +3,31 @@
 """
 Programa cliente que abre un socket a un servidor
 """
-
+import os
 import sys
 import socket
 
 # Cliente UDP simple.
-
-# Dirección IP del servidor.
-SERVER = 'localhost'
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
 try:
-    if 'INVITE'!= sys.argv[1] :
+    if 'INVITE' != sys.argv[1]:
         if 'BYE' != sys.argv[1]:
-            raise IndexError  
-    len(sys.argv) != 3    
+            raise IndexError
+    if len(sys.argv) != 3:
+        raise IndexError
 except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
 if sys.argv[1] == 'INVITE' or 'BYE':
-    LINE = sys.argv[1] + ' sip:' + sys.argv[2][:sys.argv[2].rfind(':')] + ' SIP/2.0\r\n'
-
+    LINE = sys.argv[1] + ' sip:'
+    LINE += sys.argv[2][:sys.argv[2].rfind(':')] + ' SIP/2.0\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     PORT = int(sys.argv[2][sys.argv[2].rfind(':')+1:])
+    SERVER = str(sys.argv[2][sys.argv[2].find('@')+1:sys.argv[2].rfind(':')])
     my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
@@ -42,8 +36,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         if 'Trying'in mensaje:
             if 'Ringing'in mensaje:
                 if 'OK' in mensaje:
-                    LINE = 'ACK '+ ' sip:' + sys.argv[2][:sys.argv[2].rfind(':')] + ' SIP/2.0\r\n'
+                    LINE = 'ACK' + ' sip:'
+                    LINE += sys.argv[2][:sys.argv[2].rfind(':')]
+                    LINE += ' SIP/2.0\r\n'
             my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 
     print(data.decode('utf-8'))
-
