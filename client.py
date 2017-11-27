2f77c7af676e7fc359c0cec998a8fac163980a20
Nieves Canas Martin <nievescm@gamma13.aulas.gsyc.urjc.es>
2017-11-27 09:54:45 +0100

    Añadido comentarios

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import os
 import sys
 import socket
 
-# Cliente UDP simple.
+# Errores: Entrada de linea de comandos y existencia de fichero.
 try:
     if 'INVITE' != sys.argv[1]:
         if 'BYE' != sys.argv[1]:
@@ -17,11 +17,12 @@ try:
 except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
+# Crea el mensaje.
 if sys.argv[1] == 'INVITE' or 'BYE':
     LINE = sys.argv[1] + ' sip:'
     LINE += sys.argv[2][:sys.argv[2].rfind(':')] + ' SIP/2.0\r\n'
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+# Creamos el socket, lo configuramos, lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     PORT = int(sys.argv[2][sys.argv[2].rfind(':')+1:])
@@ -31,6 +32,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
+    # Condicionamos los mensajes ACK.
     if data.decode('utf-8') != '':
         mensaje = (data.decode('utf-8').split())
         if 'Trying'in mensaje:
